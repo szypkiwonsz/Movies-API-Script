cdd33db803f880b0d1eaa709fb5d83c65ad2f233
szypkiwonsz <kacpersawicki321@gmail.com>
2020-05-07 20:39:00 +0200

    Added add_movie argument

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 from database import Api
-from movies import SortBy, FilterBy, CompareBy
+from movies import SortBy, FilterBy, CompareBy, AddMovie
 import argparse
 import time
 
@@ -15,6 +15,7 @@ if __name__ == "__main__":
     parser.add_argument("--sort_by", help="sort movies by column")
     parser.add_argument("--filter_by", nargs='+', help="sort movies by column")
     parser.add_argument("--compare_by", nargs='+', help="comparing movies by column")
+    parser.add_argument("--add_movie", help="adding movie by title")
     args = parser.parse_args()
 
     if not any(vars(args).values()):
@@ -31,5 +32,8 @@ if __name__ == "__main__":
     elif args.compare_by:
         compare_by = CompareBy('movies.sqlite', args.compare_by)
         compare_by.compare_by()
+    elif args.add_movie:
+        add_movie = AddMovie('movies.sqlite', args.add_movie)
+        add_movie.add_movie()
         finish = time.perf_counter()
         print(f'Finished in {round(finish-start, 2)} second(s)')
