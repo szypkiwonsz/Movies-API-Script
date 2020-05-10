2c935790adcc967d432b7a0524f8fc1ec6d73862
szypkiwonsz <kacpersawicki321@gmail.com>
2020-05-10 12:24:41 +0200

    Commented time performance check

--- script.py
+++ script.py
@@ -3,24 +3,24 @@ from movies import SortBy, FilterBy, CompareBy, AddMovie, Highscores
 import argparse
 import time
 
-if __name__ == "__main__":
-    start = time.perf_counter()
+if __name__ == '__main__':
+    # start = time.perf_counter()
 
     API_KEY = '39f41e43'
     DB_NAME = 'movies.sqlite'
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--get_data", action='store_true',
-                        help="download all movies")
-    parser.add_argument("--sort_by", help="sort movies by column")
-    parser.add_argument("--filter_by", nargs='+', help="sort movies by column")
-    parser.add_argument("--compare_by", nargs='+', help="comparing movies by column")
-    parser.add_argument("--add_movie", help="adding movie by title")
-    parser.add_argument("--highscores", action='store_true', help="shows highscores")
+    parser.add_argument('--get_data', action='store_true',
+                        help='download all movies')
+    parser.add_argument('--sort_by', help='sort movies by column')
+    parser.add_argument('--filter_by', nargs='+', help='sort movies by column')
+    parser.add_argument('--compare_by', nargs='+', help='comparing movies by column')
+    parser.add_argument('--add_movie', help='adding movie by title')
+    parser.add_argument('--highscores', action='store_true', help='shows highscores')
     args = parser.parse_args()
 
     if not any(vars(args).values()):
-        print("There are no arguments passed!")
+        print('There are no arguments passed!')
     elif args.get_data:
         api = Api(DB_NAME, API_KEY)
         api.update_database()
@@ -42,5 +42,5 @@ if __name__ == "__main__":
     elif args.highscores:
         highscores = Highscores(DB_NAME)
         highscores.highscores()
-        finish = time.perf_counter()
-        print(f'Finished in {round(finish-start, 2)} second(s)')
+        # finish = time.perf_counter()
+        # print(f'Finished in {round(finish-start, 2)} second(s)')
